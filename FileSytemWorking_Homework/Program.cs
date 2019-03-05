959efe1c424148fbad24c8884caec06d508473e8
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-03-06 01:26:39 +0600

    Добавьте файлы проекта.

---
+++ FileSytemWorking_Homework/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSytemWorking_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.В файле записана непустая последовательность целых чисел, являющихся числами Фибоначчи. 
+             * Приписать еще столько же чисел этой последовательности.*/
+
+            string path = @"D:\input.txt";
+
+            Console.Write("Введите количество последовательности чисел: ");
+            int numderFibonachi = int.Parse(Console.ReadLine());
+            int nFibonachi = 0;
+
+            for (int i = 0; i < numderFibonachi; i++)
+            {
+                nFibonachi = Fibonachi(i);
+                Console.Write(nFibonachi+"\t");
+
+                using(StreamWriter sw=new StreamWriter(path, true, System.Text.Encoding.Default))
+                {
+
+                    sw.Write(nFibonachi+"\t");
+                }
+            }
+
+            Console.WriteLine("\n Приписать еще столько же чисел этой последовательности");
+
+            int oldNFibonachi = nFibonachi+2; 
+
+            using (StreamWriter sw=new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine("\n");
+            }
+            for (int i = oldNFibonachi; i < numderFibonachi+ oldNFibonachi; i++)
+            {
+                nFibonachi = Fibonachi(i);
+                Console.Write(nFibonachi + "\t");
+
+                using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nFibonachi + "\t");
+                }
+            }
+        }
+
+        static int Fibonachi(int nFibonachi)
+        {
+            if (nFibonachi==0)
+            {
+                return 0;
+            }
+            else if (nFibonachi == 1)
+            {
+                return 1;
+            }
+            return Fibonachi(nFibonachi - 1) + Fibonachi(nFibonachi - 2);
+        }
+    }
+}
---
+++ FileSytemWorking_Homework2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSytemWorking_Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2.Сложить два целых числа А и В.
+             * Входные данные
+             * В единственной строке входного файла INPUT.TXT записано два натуральных числа через пробел.
+             * Выходные данные
+             * В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел А и В.*/
+
+            string inputPath = @"D:\input.txt";
+            string outputPath = @"D:\output.txt";
+
+            Console.Write("Введите два числа который необходимо сохранить в файл input.txt\n" +
+                "Введите число: ");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.Write("\nЕще одно: ");
+            int secondNum = int.Parse(Console.ReadLine());
+
+            using(StreamWriter sw=new StreamWriter(inputPath, false, System.Text.Encoding.Default))
+            {
+                sw.Write(firstNum);
+                sw.Write("\n"+ secondNum);
+            }
+
+            using(StreamReader sr=new StreamReader(inputPath, System.Text.Encoding.Default))
+            {
+                string line;
+                int count = 0;
+                while ((line=sr.ReadLine())!=null)
+                {
+                    if (count == 0)
+                    {
+                        firstNum = int.Parse(line);
+                    }
+                    else
+                    {
+                        secondNum = int.Parse(line);
+                    }
+                    ++count;
+                }
+            }
+
+            int resultingValue = firstNum + secondNum;
+            Console.WriteLine(resultingValue);
+            using (StreamWriter sw = new StreamWriter(outputPath, false, System.Text.Encoding.Default))
+            {
+                sw.Write(resultingValue);
+            }
+        }
+    }
+}
